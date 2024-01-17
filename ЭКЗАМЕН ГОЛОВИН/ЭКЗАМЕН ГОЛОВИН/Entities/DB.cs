199e0e47ea3ddac08966627f1a7bc90598fd3b1f
User <you@example.com>
2024-01-17 16:07:19 +0300

    asd

---
+++ ЭКЗАМЕН ГОЛОВИН/ЭКЗАМЕН ГОЛОВИН/Entities/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЭКЗАМЕН_ГОЛОВИН.Entities
+{
+    public class DB
+    {
+        public static OOO_RULEntities1 entities1 = new OOO_RULEntities1();
+    }
+}
